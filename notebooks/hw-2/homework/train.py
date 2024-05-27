4a78d8eb5abe9dc0e75c610b61c49030580f3a45
Avyukth <subhrajit.makur@hotmail.com>
2024-05-28 05:03:05 +0530

    final hw-2

---
+++ notebooks/hw-2/homework/train.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+import mlflow
+import click
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("random-forest-train")
+parent_directory = "/Users/amrit/Documents/Projects/DataTalkClub/cohort/mlops-Zoomcamp"
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    mlflow.sklearn.autolog()
+
+    with mlflow.start_run():
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
+        # artifact_path = "artifacts/models"
+        local_path = parent_directory + "/models/model.bin"
+
+        mlflow.log_artifact(local_path=local_path)
+
+
+if __name__ == '__main__':
+    run_train()
+
